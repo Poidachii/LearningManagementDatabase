f588a09a40b6d8a404879b8bb4854cbe4aa6b274
Poidachii <guiennebernard@gmail.com>
2023-11-21 18:39:31 +0800

    admincs and accounthandlingcs

--- GUI(yanirevised)/GUI/Admin/Admin.Designer.cs
+++ GUI(yanirevised)/GUI/Admin/Admin.Designer.cs
@@ -28,34 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label11 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.panel15 = new System.Windows.Forms.Panel();
             this.label15 = new System.Windows.Forms.Label();
-            this.AccHandleButton = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.panel4 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
-            this.button6 = new System.Windows.Forms.Button();
             this.panel5 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.panel12 = new System.Windows.Forms.Panel();
             this.label12 = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.AccHandleButton = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AccountDataViewer = new System.Windows.Forms.DataGridView();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel15.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel6.SuspendLayout();
             this.panel12.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDataViewer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -82,18 +89,6 @@
             this.label11.TabIndex = 3;
             this.label11.Text = "ACCOUNT";
             // 
-            // button1
-            // 
-            this.button1.BackgroundImage = global::GUI.Properties.Resources.account_logo_png_11;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(789, 3);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(50, 38);
-            this.button1.TabIndex = 1;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -108,8 +103,11 @@
             // 
             // panel15
             // 
-            this.panel15.BackColor = System.Drawing.Color.Transparent;
+            this.panel15.BackColor = System.Drawing.Color.RoyalBlue;
             this.panel15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel15.Controls.Add(this.pictureBox1);
+            this.panel15.Controls.Add(this.SearchBar);
+            this.panel15.Controls.Add(this.AddButton);
             this.panel15.Controls.Add(this.label15);
             this.panel15.Location = new System.Drawing.Point(224, 46);
             this.panel15.Margin = new System.Windows.Forms.Padding(2);
@@ -120,39 +118,13 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(33, 17);
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(21, 12);
             this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(73, 13);
+            this.label15.Size = new System.Drawing.Size(109, 20);
             this.label15.TabIndex = 1;
-            this.label15.Text = "SUMMARY ";
-            // 
-            // AccHandleButton
-            // 
-            this.AccHandleButton.BackgroundImage = global::GUI.Properties.Resources.acc_handling;
-            this.AccHandleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.AccHandleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.AccHandleButton.Location = new System.Drawing.Point(0, 0);
-            this.AccHandleButton.Margin = new System.Windows.Forms.Padding(2);
-            this.AccHandleButton.Name = "AccHandleButton";
-            this.AccHandleButton.Size = new System.Drawing.Size(217, 49);
-            this.AccHandleButton.TabIndex = 0;
-            this.AccHandleButton.UseVisualStyleBackColor = true;
-            this.AccHandleButton.Click += new System.EventHandler(this.AccHandleButton_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackgroundImage = global::GUI.Properties.Resources.course_management;
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(1, 0);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(217, 50);
-            this.button3.TabIndex = 1;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label15.Text = "Account List";
             // 
             // panel2
             // 
@@ -167,17 +139,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(221, 542);
             this.panel2.TabIndex = 51;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackgroundImage = global::GUI.Properties.Resources.MMCL_Logo_original;
-            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox6.Location = new System.Drawing.Point(5, 4);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(211, 111);
-            this.pictureBox6.TabIndex = 17;
-            this.pictureBox6.TabStop = false;
             // 
             // panel4
             // 
@@ -202,18 +163,6 @@
             this.label3.Size = new System.Drawing.Size(44, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "HOME";
-            // 
-            // button6
-            // 
-            this.button6.BackgroundImage = global::GUI.Properties.Resources.img_66574;
-            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(0, 0);
-            this.button6.Margin = new System.Windows.Forms.Padding(2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(217, 50);
-            this.button6.TabIndex = 3;
-            this.button6.UseVisualStyleBackColor = true;
             // 
             // panel5
             // 
@@ -290,6 +239,55 @@
             this.label12.TabIndex = 7;
             this.label12.Text = "CALENDAR";
             // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::GUI.Properties.Resources.MMCL_Logo_original;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox6.Location = new System.Drawing.Point(5, 4);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(211, 111);
+            this.pictureBox6.TabIndex = 17;
+            this.pictureBox6.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::GUI.Properties.Resources.img_66574;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(0, 0);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(217, 50);
+            this.button6.TabIndex = 3;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::GUI.Properties.Resources.course_management;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(1, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(217, 50);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // AccHandleButton
+            // 
+            this.AccHandleButton.BackgroundImage = global::GUI.Properties.Resources.acc_handling;
+            this.AccHandleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.AccHandleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AccHandleButton.Location = new System.Drawing.Point(0, 0);
+            this.AccHandleButton.Margin = new System.Windows.Forms.Padding(2);
+            this.AccHandleButton.Name = "AccHandleButton";
+            this.AccHandleButton.Size = new System.Drawing.Size(217, 49);
+            this.AccHandleButton.TabIndex = 0;
+            this.AccHandleButton.UseVisualStyleBackColor = true;
+            this.AccHandleButton.Click += new System.EventHandler(this.AccHandleButton_Click);
+            // 
             // button10
             // 
             this.button10.BackgroundImage = global::GUI.Properties.Resources.calendar_icon_png_transparent_3;
@@ -302,11 +300,65 @@
             this.button10.TabIndex = 8;
             this.button10.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::GUI.Properties.Resources.account_logo_png_11;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(789, 3);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 38);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AccountDataViewer
+            // 
+            this.AccountDataViewer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.AccountDataViewer.Location = new System.Drawing.Point(227, 99);
+            this.AccountDataViewer.Name = "AccountDataViewer";
+            this.AccountDataViewer.Size = new System.Drawing.Size(715, 471);
+            this.AccountDataViewer.TabIndex = 52;
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.AliceBlue;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(152, 9);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(107, 26);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "ADD";
+            this.AddButton.UseVisualStyleBackColor = false;
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.Location = new System.Drawing.Point(370, 12);
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(282, 20);
+            this.SearchBar.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(658, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(954, 582);
+            this.Controls.Add(this.AccountDataViewer);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel15);
@@ -319,7 +371,6 @@
             this.panel15.ResumeLayout(false);
             this.panel15.PerformLayout();
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel5.ResumeLayout(false);
@@ -328,6 +379,9 @@
             this.panel6.PerformLayout();
             this.panel12.ResumeLayout(false);
             this.panel12.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AccountDataViewer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -353,5 +407,9 @@
         private System.Windows.Forms.Panel panel12;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox SearchBar;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.DataGridView AccountDataViewer;
     }
 }
