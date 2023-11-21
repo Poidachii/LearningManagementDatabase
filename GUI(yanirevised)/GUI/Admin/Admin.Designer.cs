59b32c12de2ecdc51edc50c394645f8d1108539d
Poidachii <guiennebernard@gmail.com>
2023-11-21 18:09:59 +0800

    admin.cs change

--- GUI(yanirevised)/GUI/Admin/Admin.Designer.cs
+++ GUI(yanirevised)/GUI/Admin/Admin.Designer.cs
@@ -36,8 +36,6 @@
             this.label15 = new System.Windows.Forms.Label();
             this.AccHandleButton = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.DBbutton = new System.Windows.Forms.Button();
-            this.QuizManagementBtn = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.panel4 = new System.Windows.Forms.Panel();
@@ -47,13 +45,9 @@
             this.label4 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
             this.panel12 = new System.Windows.Forms.Panel();
             this.label12 = new System.Windows.Forms.Label();
             this.button10 = new System.Windows.Forms.Button();
-            this.panel13 = new System.Windows.Forms.Panel();
-            this.label13 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel15.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -61,9 +55,7 @@
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel6.SuspendLayout();
-            this.panel7.SuspendLayout();
             this.panel12.SuspendLayout();
-            this.panel13.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -119,7 +111,7 @@
             this.panel15.BackColor = System.Drawing.Color.Transparent;
             this.panel15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel15.Controls.Add(this.label15);
-            this.panel15.Location = new System.Drawing.Point(224, 51);
+            this.panel15.Location = new System.Drawing.Point(224, 46);
             this.panel15.Margin = new System.Windows.Forms.Padding(2);
             this.panel15.Name = "panel15";
             this.panel15.Size = new System.Drawing.Size(723, 48);
@@ -141,10 +133,10 @@
             this.AccHandleButton.BackgroundImage = global::GUI.Properties.Resources.acc_handling;
             this.AccHandleButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.AccHandleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.AccHandleButton.Location = new System.Drawing.Point(-2, -2);
+            this.AccHandleButton.Location = new System.Drawing.Point(0, 0);
             this.AccHandleButton.Margin = new System.Windows.Forms.Padding(2);
             this.AccHandleButton.Name = "AccHandleButton";
-            this.AccHandleButton.Size = new System.Drawing.Size(221, 51);
+            this.AccHandleButton.Size = new System.Drawing.Size(217, 49);
             this.AccHandleButton.TabIndex = 0;
             this.AccHandleButton.UseVisualStyleBackColor = true;
             this.AccHandleButton.Click += new System.EventHandler(this.AccHandleButton_Click);
@@ -154,38 +146,13 @@
             this.button3.BackgroundImage = global::GUI.Properties.Resources.course_management;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(-3, -2);
+            this.button3.Location = new System.Drawing.Point(1, 0);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(221, 51);
+            this.button3.Size = new System.Drawing.Size(217, 50);
             this.button3.TabIndex = 1;
             this.button3.UseVisualStyleBackColor = true;
-            // 
-            // DBbutton
-            // 
-            this.DBbutton.BackgroundImage = global::GUI.Properties.Resources.DEV_TOOL;
-            this.DBbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.DBbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.DBbutton.Location = new System.Drawing.Point(-1, -1);
-            this.DBbutton.Margin = new System.Windows.Forms.Padding(2);
-            this.DBbutton.Name = "DBbutton";
-            this.DBbutton.Size = new System.Drawing.Size(221, 51);
-            this.DBbutton.TabIndex = 0;
-            this.DBbutton.UseVisualStyleBackColor = true;
-            this.DBbutton.Click += new System.EventHandler(this.DBbutton_Click);
-            // 
-            // QuizManagementBtn
-            // 
-            this.QuizManagementBtn.BackgroundImage = global::GUI.Properties.Resources._6343947;
-            this.QuizManagementBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.QuizManagementBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.QuizManagementBtn.Location = new System.Drawing.Point(-1, -1);
-            this.QuizManagementBtn.Margin = new System.Windows.Forms.Padding(2);
-            this.QuizManagementBtn.Name = "QuizManagementBtn";
-            this.QuizManagementBtn.Size = new System.Drawing.Size(221, 51);
-            this.QuizManagementBtn.TabIndex = 0;
-            this.QuizManagementBtn.UseVisualStyleBackColor = true;
-            this.QuizManagementBtn.Click += new System.EventHandler(this.QuizManagementBtn_Click);
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // panel2
             // 
@@ -194,9 +161,7 @@
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Controls.Add(this.panel6);
-            this.panel2.Controls.Add(this.panel7);
             this.panel2.Controls.Add(this.panel12);
-            this.panel2.Controls.Add(this.panel13);
             this.panel2.Location = new System.Drawing.Point(-1, 42);
             this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
@@ -220,7 +185,7 @@
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel4.Controls.Add(this.label3);
             this.panel4.Controls.Add(this.button6);
-            this.panel4.Location = new System.Drawing.Point(0, 118);
+            this.panel4.Location = new System.Drawing.Point(0, 119);
             this.panel4.Margin = new System.Windows.Forms.Padding(2);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(222, 53);
@@ -231,7 +196,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(-3, 0);
+            this.label3.Location = new System.Drawing.Point(2, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 15);
@@ -243,10 +208,10 @@
             this.button6.BackgroundImage = global::GUI.Properties.Resources.img_66574;
             this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Location = new System.Drawing.Point(-2, -1);
+            this.button6.Location = new System.Drawing.Point(0, 0);
             this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(221, 51);
+            this.button6.Size = new System.Drawing.Size(217, 50);
             this.button6.TabIndex = 3;
             this.button6.UseVisualStyleBackColor = true;
             // 
@@ -256,7 +221,7 @@
             this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel5.Controls.Add(this.label4);
             this.panel5.Controls.Add(this.button3);
-            this.panel5.Location = new System.Drawing.Point(0, 172);
+            this.panel5.Location = new System.Drawing.Point(-1, 173);
             this.panel5.Margin = new System.Windows.Forms.Padding(2);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(224, 53);
@@ -270,12 +235,12 @@
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(-3, 0);
+            this.label4.Location = new System.Drawing.Point(-2, 0);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 15);
+            this.label4.Size = new System.Drawing.Size(112, 15);
             this.label4.TabIndex = 5;
-            this.label4.Text = "COURSE SET.";
+            this.label4.Text = "COURSE SETTING";
             // 
             // panel6
             // 
@@ -297,33 +262,9 @@
             this.label5.Location = new System.Drawing.Point(-2, 0);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 15);
+            this.label5.Size = new System.Drawing.Size(118, 15);
             this.label5.TabIndex = 3;
-            this.label5.Text = "ACCOUNT SET.";
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel7.Controls.Add(this.label6);
-            this.panel7.Controls.Add(this.QuizManagementBtn);
-            this.panel7.Location = new System.Drawing.Point(0, 282);
-            this.panel7.Margin = new System.Windows.Forms.Padding(2);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(224, 53);
-            this.panel7.TabIndex = 4;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(-1, 1);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(66, 15);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "TEST SET.";
+            this.label5.Text = "ACCOUNT SETTING";
             // 
             // panel12
             // 
@@ -331,7 +272,7 @@
             this.panel12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.panel12.Controls.Add(this.label12);
             this.panel12.Controls.Add(this.button10);
-            this.panel12.Location = new System.Drawing.Point(0, 337);
+            this.panel12.Location = new System.Drawing.Point(0, 282);
             this.panel12.Margin = new System.Windows.Forms.Padding(2);
             this.panel12.Name = "panel12";
             this.panel12.Size = new System.Drawing.Size(222, 53);
@@ -342,7 +283,7 @@
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label12.Location = new System.Drawing.Point(-1, 0);
+            this.label12.Location = new System.Drawing.Point(0, 0);
             this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(71, 15);
@@ -354,36 +295,12 @@
             this.button10.BackgroundImage = global::GUI.Properties.Resources.calendar_icon_png_transparent_3;
             this.button10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button10.Location = new System.Drawing.Point(-1, -1);
+            this.button10.Location = new System.Drawing.Point(0, -4);
             this.button10.Margin = new System.Windows.Forms.Padding(2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(221, 51);
+            this.button10.Size = new System.Drawing.Size(217, 51);
             this.button10.TabIndex = 8;
             this.button10.UseVisualStyleBackColor = true;
-            // 
-            // panel13
-            // 
-            this.panel13.BackColor = System.Drawing.Color.RoyalBlue;
-            this.panel13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel13.Controls.Add(this.label13);
-            this.panel13.Controls.Add(this.DBbutton);
-            this.panel13.Location = new System.Drawing.Point(0, 393);
-            this.panel13.Margin = new System.Windows.Forms.Padding(2);
-            this.panel13.Name = "panel13";
-            this.panel13.Size = new System.Drawing.Size(222, 53);
-            this.panel13.TabIndex = 18;
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label13.Location = new System.Drawing.Point(-1, 0);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(68, 15);
-            this.label13.TabIndex = 7;
-            this.label13.Text = "DATABASE";
             // 
             // Admin
             // 
@@ -409,12 +326,8 @@
             this.panel5.PerformLayout();
             this.panel6.ResumeLayout(false);
             this.panel6.PerformLayout();
-            this.panel7.ResumeLayout(false);
-            this.panel7.PerformLayout();
             this.panel12.ResumeLayout(false);
             this.panel12.PerformLayout();
-            this.panel13.ResumeLayout(false);
-            this.panel13.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -428,8 +341,6 @@
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Button AccHandleButton;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button DBbutton;
-        private System.Windows.Forms.Button QuizManagementBtn;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.Panel panel4;
@@ -439,12 +350,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Panel panel12;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Panel panel13;
-        private System.Windows.Forms.Label label13;
     }
 }
